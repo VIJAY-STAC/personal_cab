6dfcf16794970f895e6333153d132c10fdeb8576
Vijay <vijay.thorat@biddano.com>
2023-10-17 19:56:55 +0530

    updated start_app.sh file

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Change to the correct directory where your application code is located
-cd /home/ubuntu/
-source /env/bin/activate
+# Activate the virtual environment (change to your virtual environment path)
+pwd
+echo "hello1 "
+source /home/ubuntu/env/bin/activate
+echo "hello2 "
+pwd
+# Change to the directory where your Django project is located
+cd /home/ubuntu/personal_car
 
-cd /personal_car
-
-# Update settings.py file
+# Update the settings.py file
 sed -i 's/\[]/\["13.232.225.109"]/' cars/settings.py
 
 # Print the current working directory for debugging
@@ -14,9 +17,11 @@ pwd
 
 # Start the application commands
 echo "Starting the app"
-python3 manage.py migrate 
+python manage.py migrate 
 echo "Migrations done"
-python3 manage.py makemigrations     
-python3 manage.py collectstatic
+python manage.py makemigrations
+python manage.py collectstatic
+
+# Restart services
 sudo service gunicorn restart
 sudo service nginx restart
