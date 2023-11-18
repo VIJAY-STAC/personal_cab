3bb9e2924cf0abe4d5e3c7ac9ff3ff01b841f27d
Vijay <vijay.thorat@biddano.com>
2023-11-19 01:27:34 +0530

    updated requirement file

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,5 +3,7 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/personal_cab/requirements.txt
+sudo apt-get install -y libpq-dev
+pip install psycopg2
 cd /home/ubuntu/env/bin
 pip install gunicorn
